1256619e2ec541d32b666ded4fd4bb7a03e1f3d5
Maze <invoker322sf@gmail.com>
2021-06-10 18:51:12 +1000

    add custom messages

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ user_id = input("Введите ID пользователя: ")
 
 history = messages.method('messages.getHistory', user_id=user_id, count=1)
 
-message_to_send = 'нет ты'
-
+message_to_search = input("Введите сообщение на которое будем отвечать:\n").lower()
+message_to_send = input("Введите сообщение которое будем отправлять:\n")
 
 while True:
-    history = messages.method('messages.getHistory', user_id='201675606', count=1)
+    history = messages.method('messages.getHistory', user_id=user_id, count=1)
     last_msg_text = history['items'][0]['text']
     last_msg_id = history['items'][0]['from_id']
     if last_msg_id != from_id:
-        if last_msg_text.lower() == 'нет ты':
-            messages.method('messages.send', user_id='201675606', message=message_to_send, random_id=get_random())
+        if last_msg_text == message_to_search:
+            messages.method('messages.send', user_id=user_id, message=message_to_send, random_id=get_random())
             print(f'Отправлено сообщение "{message_to_send}"')
