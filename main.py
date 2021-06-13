b0b9f964d69416bd855c9dc9dd0a9c09e63d3f6f
Maze <invoker322sf@gmail.com>
2021-06-13 22:48:56 +1000

    some fixes

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from settings import LOGIN, PASSWORD, TT_KEY
 from get_objects import *
 from sys import exit
 from time import sleep, time
-from threading import Thread
 import vk_api
 
 
@@ -14,7 +13,7 @@ def get_img(path):
     attachments = list()
     upload_image = upload.photo_messages(photos=image)[0]
     attachments.append(f'photo{upload_image["owner_id"]}_{upload_image["id"]}')
-    return attachments
+    return ','.join(attachments)
 
 
 def create_help():
@@ -27,60 +26,114 @@ def create_help():
     return help_msg
 
 
-def do_request(id_user):
-    #  Получаем 5 последних сообщений с пользователем id_user
-    history = messages.method('messages.getHistory', user_id=id_user, count=5)
+def fill_commands_list(history, i):
+    global commands
     all_msg_logs = open('all_msg_logs.txt', 'a+', encoding='utf-8')
-    #  Если ID отправителя этого сообщения не равен нашему
-    for i in reversed(range(5)):
-        last_msg_text = history['items'][i]['text']
-        last_msg_id = history['items'][i]['from_id']
-        msg_id = history['items'][i]['id']
-        #  Добавляем ID сообщения во множество, что бы не отвечать повторно на сообщения по нескольку раз
-        if msg_id in msg_ids_set:
-            continue
-        all_msg_logs.write(datetime.now().strftime("<%d-%m-%Y %H:%M:%S> "))
-        all_msg_logs.write(f'"{last_msg_text}" {vk.users.get(user_id=last_msg_id)[0]["first_name"]}'
-                           f' {vk.users.get(user_id=last_msg_id)[0]["last_name"]}, MSG_ID: {msg_id}\n')
+    last_msg_text = history['items'][i]['text']
+    last_msg_id = history['items'][i]['from_id']
+    msg_id = history['items'][i]['id']
+    if msg_id in msg_ids_set:
+        return
+    all_msg_logs.write(datetime.now().strftime("<%d-%m-%Y %H:%M:%S> "))
+    all_msg_logs.write(f'"{last_msg_text}" {vk.users.get(user_id=last_msg_id)[0]["first_name"]}'
+                       f' {vk.users.get(user_id=last_msg_id)[0]["last_name"]}, MSG_ID: {msg_id}\n')
+    msg_ids_set.add(msg_id)
+    if last_msg_text.lower() in ['!выкл', '!пауза']:
+        if last_msg_id == my_id:
+            commands.append(last_msg_text.lower())
+            msg_ids_set.add(msg_id)
+            return
+    elif last_msg_text.lower() in ['!мем', '!команды']:
+        commands.append(last_msg_text.lower())
         msg_ids_set.add(msg_id)
-        #  Распечатка всех возможных команд
-        if last_msg_text.lower() == '!команды':
-            messages.method('messages.send', peer_id=id_user, message=create_help(), random_id=get_random())
-            msg_ids_set.add(msg_id)
-            print(f'{datetime.now().strftime("<%d-%m-%Y %H:%M:%S> ")} отправлены команды')
-            continue
-        elif last_msg_text.lower() == '!мем':
-            messages.method('messages.send', peer_id=id_user, attachment=','.join(get_img(mem_list)),
-                            random_id=get_random())
-            msg_ids_set.add(msg_id)
-            print(f'{datetime.now().strftime("<%d-%m-%Y %H:%M:%S> ")} отправлена картинка')
-            continue
-        elif last_msg_text.lower() == 'ab6dfb89f46d898bb3896f397fd801cf363c3c2559e8dc65cc2b96445257b262':
-            messages.method('messages.send', peer_id=id_user, message='все бот умер', random_id=get_random())
-            exit()
-        #  Если последнее сообщение не от нас
-        elif last_msg_id != my_id:
-            #  Функция разбивает сообщение по пробелам, обходит все слова, если они есть в словаре - возращает ключ
-            #  Иначе возвращает False
-            last_msg_text = get_key(last_msg_text, answers)
-            if last_msg_text:
-                #  Если по ключу находится список, то мы выбираем случайный его элемент
-                if isinstance(answers[last_msg_text], list):
-                    message = choice(answers[last_msg_text])
+        return
+    elif last_msg_id != my_id:
+        temp_dictionary = dict()
+        last_msg_text = get_key(last_msg_text, answers)
+        if last_msg_text:
+            if isinstance(answers[last_msg_text], list):
+                temp_dictionary['message'] = choice(answers[last_msg_text])
+            else:
+                temp_dictionary['message'] = answers[last_msg_text.lower()]
+            temp_dictionary['first_name'] = vk.users.get(name_case="dat", user_id=last_msg_id)[0]["first_name"]
+            temp_dictionary['last_name'] = vk.users.get(name_case="dat", user_id=last_msg_id)[0]["last_name"]
+            temp_dictionary['msg_id'] = msg_id
+            temp_dictionary['last_msg_id'] = last_msg_id
+        commands.append(temp_dictionary)
+        msg_ids_set.add(msg_id)
+    all_msg_logs.close()
+    return
+
+
+def do_request(id_user):
+    global commands, active
+    n = 5
+    #  Получаем 5 последних сообщений с пользователем id_user
+    history = messages.method('messages.getHistory', user_id=id_user, count=n)
+
+    commands = list()
+    for i in range(n):
+        fill_commands_list(history, i)
+
+    for i, cmd in enumerate(reversed(commands)):
+
+        if isinstance(cmd, str):
+            if cmd == '!пауза':
+                if active:
+                    message = 'поставил на паузу'
+                    active = False
                 else:
-                    message = answers[last_msg_text.lower()]
-                firstname = vk.users.get(name_case="dat", user_id=last_msg_id)[0]["first_name"]
-                lastname = vk.users.get(name_case="dat", user_id=last_msg_id)[0]["last_name"]
-                messages.method('messages.send', peer_id=id_user, message=message,
-                                random_id=get_random(), reply_to=msg_id)
-                msg_ids_set.add(msg_id)
+                    message = 'снял с паузы'
+                    active = True
+                messages.method(name='messages.send',
+                                peer_id=id_user,
+                                message=message,
+                                random_id=get_random())
+                print(f'{datetime.now().strftime("<%d-%m-%Y %H:%M:%S> ")} {message}')
+
+            if cmd == '!выкл':
+                messages.method(name='messages.send',
+                                peer_id=id_user,
+                                message='все, пиздец, бот умер',
+                                random_id=get_random())
+                print(f'{datetime.now().strftime("<%d-%m-%Y %H:%M:%S> ")} отключение бота')
+                exit()
+            if active:
+                if cmd == '!мем':
+                    messages.method(name='messages.send',
+                                    peer_id=id_user,
+                                    attachment=get_img(mem_list),
+                                    random_id=get_random())
+                    print(f'{datetime.now().strftime("<%d-%m-%Y %H:%M:%S>")} отправлена картинка')
+
+                if cmd == '!команды':
+                    messages.method(name='messages.send',
+                                    peer_id=id_user,
+                                    message=create_help(),
+                                    random_id=get_random())
+                    print(f'{datetime.now().strftime("<%d-%m-%Y %H:%M:%S>")} отправлены команды')
+
+        if isinstance(cmd, dict):
+            if active:
+                try:
+                    firstname = cmd['first_name']
+                except KeyError:
+                    continue
+                lastname = cmd['last_name']
+                message = cmd['message']
+                msg_id = cmd['msg_id']
+                last_msg_id = cmd['last_msg_id']
+                messages.method('messages.send',
+                                peer_id=id_user,
+                                message=message,
+                                random_id=get_random(),
+                                reply_to=msg_id)
                 print(f'Отправлено сообщение "{message}" {firstname} {lastname}')
                 logfile = open('log.txt', 'a+', encoding='utf-8')
                 logfile.write(datetime.now().strftime("<%d-%m-%Y %H:%M:%S> "))
                 logfile.write(f'Отправлено сообщение "{message}" {firstname} {lastname}, '
                               f'ID: {last_msg_id}\n')
                 logfile.close()
-    all_msg_logs.close()
 
 
 messages = MessagesAPI(login=LOGIN, password=PASSWORD, two_factor=False, cookies_save_path='sessions/')
@@ -100,6 +153,7 @@ answers = get_answers()
 user_id_set = get_chats()
 mem_list = listdir('memes')
 correct_user_id_set = set()
+commands = list()
 msg_ids_set = set()
 
 
@@ -120,20 +174,22 @@ for user in correct_user_id_set:
     last_name = vk.users.get(name_case='gen', user_ids=user)[0]['last_name']
     print(f'Бот активирован для {first_name} {last_name}')
 
+active = True
 
 while True:
+
     new_mem = listdir('memes')
     new_ans = get_answers()
     if new_mem != mem_list:
-        print(f'{datetime.now().strftime("<%d-%m-%Y %H:%M:%S> ")} memes update')
+        print(f'{datetime.now().strftime("<%d-%m-%Y %H:%M:%S>")} memes update')
         mem_list = new_mem
     if new_ans != answers:
         answers = new_ans
-        print(f'{datetime.now().strftime("<%d-%m-%Y %H:%M:%S> ")} answers update')
+        print(f'{datetime.now().strftime("<%d-%m-%Y %H:%M:%S>")} answers update')
     try:
         for usr_id in correct_user_id_set:
             do_request(usr_id)
     except vk_messages.Exception_MessagesAPI:
-        print(vk_messages.Exception_MessagesAPI)
+        print('лул')
         sleep(10)
 
